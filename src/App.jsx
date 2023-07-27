7e53cb52f4360ba80b3332e60099dccc81bf8866
Matvey <molodoytankist06@gmail.com>
2023-07-27 16:05:16 +0300

    Test Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const headingStyle = {
+    color: 'red',
+  };
+
+  return (
+    <div>
+      <h1 style={headingStyle}>Hello World ! :D</h1>
+    </div>
+  );
+}
+
+export default App;
